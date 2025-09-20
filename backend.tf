b216b154f767d1021eb99dfe6176a798a2b4680d
kate keiroz <katekeiroz@gmail.com>
2025-09-20 16:02:46 +0100

    folder organised

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 # state.tf
-terraform {
-  backend "s3" {
-    bucket  = "terraform-state-katequeiroz"
-    key     = "site/terraform.tfstate"
-    region  = "eu-west-1"
-    encrypt = true
+#terraform {
+ ## backend "s3" {
+  #  bucket  = "terraform-state-katequeiroz"
+ #   key     = "site/terraform.tfstate"
+  #  region  = "eu-west-1"
+  ##  encrypt = true
 
-  }
-}
+#  }
+#}
